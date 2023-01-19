9bd52856e481202bb47e3fd5b84c14d252afd774
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2023-01-20 00:50:00 +0300

    add a solution to the binary_search task

---
+++ src/binary_search/binary_search.ts
@@ -0,0 +1,30 @@
+// Алгоритм бинарного поиска элемента item: number
+// из упорядоченного списка list: number[]:
+
+export function searchNumber(list: number[], item: number): number | null {
+  if (list.length === 0 || Number.isNaN(item) || Math.abs(item) === Infinity) {
+    return null;
+  }
+
+  let result: number | null = null;
+  let startInd = 0;
+  let endInd = list.length - 1;
+
+  while (startInd <= endInd) {
+    let index = Math.floor((startInd + endInd) / 2);
+    if (list[index] === item) {
+      result = list[index];
+      break;
+    }
+    if (list[index] < item) {
+      startInd = index + 1;
+      continue;
+    }
+    if (list[index] > item) {
+      endInd = index - 1;
+      continue;
+    }
+  }
+
+  return result;
+}
